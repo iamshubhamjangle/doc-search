2baad859eb9c07996e13431c56b694894a1012f1
Shubham Jangle <jangleshubham13@gmail.com>
2024-10-17 20:19:43 +0530

    removed edgestore

--- app/(client)/_components/providers.tsx
+++ app/(client)/_components/providers.tsx
@@ -2,7 +2,6 @@
 
 import { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
-import { EdgeStoreProvider } from "@/app/_lib/edgestore";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/app/(client)/_components/theme-provider";
 import { TooltipProvider } from "@/app/(client)/_components/ui/tooltip";
@@ -11,11 +10,9 @@ const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <SessionProvider>
       <Toaster />
-      <EdgeStoreProvider>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <TooltipProvider>{children}</TooltipProvider>
-        </ThemeProvider>
-      </EdgeStoreProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <TooltipProvider>{children}</TooltipProvider>
+      </ThemeProvider>
     </SessionProvider>
   );
 };
