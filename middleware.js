8a08e6a2fdfbb115ca4d70db0a230c3f03997c90
Shubham Jangle <jangleshubham13@gmail.com>
2024-11-20 18:36:07 +0530

    fix: too many redirects

--- middleware.js
+++ middleware.js
@@ -1,13 +1,5 @@
-// export { default } from "next-auth/middleware";
-import { withAuth } from "next-auth/middleware";
+export { default } from "next-auth/middleware";
 
-export default withAuth({
-  pages: {
-    signIn: "/login",
-  },
-});
-
-// Both Client and Server Components can be protected here.
 export const config = {
-  matcher: ["/", "/settings", "/profile"],
+  matcher: ["/settings", "/profile"],
 };
